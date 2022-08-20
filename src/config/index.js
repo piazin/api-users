34f9a03f617fce90f47a2453919c34721cd7abee
piazin <92053288+piazin@users.noreply.github.com>
2022-08-19 23:05:02 -0300

    ci(config.js): environment variables

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,11 @@ module.exports = {
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
     },
+    
+    email: {
+        u: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS
+    },
     port: process.env.PORT,
     secret: process.env.SECRET
 }
